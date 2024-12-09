ea1fd22ad66864d5d30c788be3196a991e70c06b
Arend-Jan Quist <a.quist@liacs.leidenuniv.nl>
2024-12-09 15:48:31 +0100

    change runtimes to 30 mins

--- benchmark/run_benchmarks.sh
+++ benchmark/run_benchmarks.sh
@@ -6,11 +6,11 @@ do
     do
         num_qubits=$((i*10))
         echo "Simulating num_qubits = " ${num_qubits} "num_gates = " ${num_gates}
-        timeout 60 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_float_low-omega.json -c -e float -s low
+        timeout 1800 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_float_low-omega.json -c -e float -s low
         echo "Finished  low     float "
-        timeout 60 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_float_l2-omega.json -c -e float -s l2
+        timeout 1800 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_float_l2-omega.json -c -e float -s l2
         echo "Finished  l2      float "
-        timeout 60 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_qisq2_low-omega.json -c -e qisq2 -s low
+        timeout 1800 ../build/qasm/run_qasm_on_qmdd qasm_clifford_T/clifford_T_circuit_${num_qubits}_${num_gates}.qasm -j benchmark_results/${num_qubits}_${num_gates}_qisq2_low-omega.json -c -e qisq2 -s low
         echo "Finished  low     qisq2 "
         echo "Finished simulation num_qubits = " ${num_qubits} "num_gates = " ${num_gates}
     done
